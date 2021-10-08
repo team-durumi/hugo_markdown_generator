676de68714b1c9b5d4e49b54105ea66fb47d406c
sun lee <suncrud@users.noreply.github.com>
2021-10-09 04:27:49 +0900

    태깅 스크립트 저고도화. 스크립트 아웃풋 수정

--- directory_structure_to_markdown.rb
+++ directory_structure_to_markdown.rb
@@ -2,51 +2,51 @@
 require 'yaml'
 require 'json'
 
-puts "[Hugo Contents Directories 2 JSON]"
-puts "[Hugo Contents Directories 2 JSON] Script ititiated."
+puts "[Hugo Contents Directories 2 MARKDOWN]"
+puts "[Hugo Contents Directories 2 MARKDOWN] Script ititiated."
 
 unless File.exist?('metameta.yml')
-  puts "[Hugo Contents Directories 2 JSON] metameta.yml does not exist. Exiting the script."
+  puts "[Hugo Contents Directories 2 MARKDOWN] metameta.yml does not exist. Exiting the script."
   return false
 end
 
 options = YAML.load_file('metameta.yml')
 unless !options.empty? && !options["base_directory"].nil?
-  puts "[Hugo Contents Directories 2 JSON] Base directory specification required for the script to run. Please re-run the script with '-h' for help."
+  puts "[Hugo Contents Directories 2 MARKDOWN] Base directory specification required for the script to run. Please re-run the script with '-h' for help."
   return false
 end
 
 items_base_dir = File.expand_path(options["base_directory"])
 unless File.directory?(items_base_dir)
-  puts "[Hugo Contents Directories 2 JSON] Invalid directory. Exiting the script."
+  puts "[Hugo Contents Directories 2 MARKDOWN] Invalid directory. Exiting the script."
   return false
 end
 
 if options["remote_url"].nil?
-  puts "[Hugo Contents Directories 2 JSON] You have not entered any remote_url for your content files. This means that your markdown file will include local file paths."
+  puts "[Hugo Contents Directories 2 MARKDOWN] You have not entered any remote_url for your content files. This means that your markdown file will include local file paths."
   # remote url 이 있으면 hugo_content_directory_name 이 무의미할듯.
   if options["hugo_content_directory_name"].nil?
-    puts "[Hugo Contents Directories 2 JSON] You have not entered any hugo content directory name for your content files. This script will resort to default name as 'items'."
+    puts "[Hugo Contents Directories 2 MARKDOWN] You have not entered any hugo content directory name for your content files. This script will resort to default name as 'items'."
   else
-    puts "[Hugo Contents Directories 2 JSON] You have entered \"#{options["hugo_content_directory_name"]}\" as the hugo content directory name for your files. This will be prepended to your file paths."
-    puts "[Hugo Contents Directories 2 JSON] This automatically ignored any content_directory_name you might have put as options."
+    puts "[Hugo Contents Directories 2 MARKDOWN] You have entered \"#{options["hugo_content_directory_name"]}\" as the hugo content directory name for your files. This will be prepended to your file paths."
+    puts "[Hugo Contents Directories 2 MARKDOWN] This automatically ignored any content_directory_name you might have put as options."
   end
 else
-  puts "[Hugo Contents Directories 2 JSON] You have entered \"#{options["remote_url"]}\" as the remote_url for your content files. This will be prepended to your file paths."
+  puts "[Hugo Contents Directories 2 MARKDOWN] You have entered \"#{options["remote_url"]}\" as the remote_url for your content files. This will be prepended to your file paths."
 end
 
-puts "[Hugo Contents Directories 2 JSON] Proceeding with the script."
-puts "[Hugo Contents Directories 2 JSON]"
+puts "[Hugo Contents Directories 2 MARKDOWN] Proceeding with the script."
+puts "[Hugo Contents Directories 2 MARKDOWN]"
 
-puts "[Hugo Contents Directories 2 JSON] Inspecting directories under '#{items_base_dir}/'"
+puts "[Hugo Contents Directories 2 MARKDOWN] Inspecting directories under '#{items_base_dir}/'"
 # valid_children = Dir.entries(items_base_dir).reject {|dir| %w{# .}.include? dir[0]} # trying to skip '#recycle' in synology nas 
 # child_dirs = []
 # valid_children.map {|dir| items_base_dir + '/' +  dir}.each {|path| child_dirs.push(Dir.glob(path + '/**/*/'))}
 # child_dirs.flatten!
 # child_dirs.reject! {|dir| dir.include? '@eaDir'} # trying to skip '@eaDir' in synology nas 
 all_children = Dir.glob("#{items_base_dir}/**/*")
-puts "[Hugo Contents Directories 2 JSON] There are total #{all_children.count} directories and files under '#{items_base_dir}'"
-puts "[Hugo Contents Directories 2 JSON]"
+puts "[Hugo Contents Directories 2 MARKDOWN] There are total #{all_children.count} directories and files under '#{items_base_dir}'"
+puts "[Hugo Contents Directories 2 MARKDOWN]"
 
 default_index_front_matters = %w{title weight type lastmod}
 index_front_matters = (default_index_front_matters + options["front_matters"]["index"]).uniq
@@ -56,7 +56,12 @@ single_front_matters = (default_single_front_matters + options["front_matters"][
 # single_hash_template = Hash[single_front_matters.map {|x| [x, nil]}]
 
 all_children.each_with_index do |path, i|
+  puts "[Hugo Contents Directories 2 MARKDOWN] Examining #{path}"
+  puts "[Hugo Contents Directories 2 MARKDOWN]"
   if File.directory?(path)
+    puts "[Hugo Contents Directories 2 MARKDOWN] This is a directory. Proceeding accordingly..."
+    puts "[Hugo Contents Directories 2 MARKDOWN]"
+  
     # type = 'index'
     hash = Hash[index_front_matters.map {|x| [x, nil]}]
 
@@ -70,7 +75,14 @@ all_children.each_with_index do |path, i|
       f.write(YAML.dump(JSON.parse(hash.to_json)))
       f << "---\n"
     end
+
+    puts "[Hugo Contents Directories 2 MARKDOWN] Created \"#{path + '/_index.md'}\""
+    puts "[Hugo Contents Directories 2 MARKDOWN]"
+
   elsif File.file?(path)
+    puts "[Hugo Contents Directories 2 MARKDOWN] This is a file. Proceeding accordingly..."
+    puts "[Hugo Contents Directories 2 MARKDOWN]"
+
     # type = 'single'
     hash = Hash[single_front_matters.map {|x| [x, nil]}]
 
@@ -89,11 +101,11 @@ all_children.each_with_index do |path, i|
     hash["components"] = [].push(component_path)
 
     if File.extname(path) == '.pdf'
-      puts
-      puts "initiating tag process..."
-      puts "converting pdf to txt"
+      puts "[Hugo Contents Directories 2 MARKDOWN] initiating tag process..."
+      puts "[Hugo Contents Directories 2 MARKDOWN] converting pdf to txt"
       `ruby ./pdf2txt.rb "#{path}"`
-      puts "attempting to get natto"
+      puts "[Hugo Contents Directories 2 MARKDOWN] attempting to get natto"
+
       require 'natto'
       text = File.read(path + ".txt")
       nm = Natto::MeCab.new
@@ -101,19 +113,25 @@ all_children.each_with_index do |path, i|
       # puts result
       tag_array = []
       result.each do |part|
-        if ['NNG', 'NNP'].include?(part.feature.split(',').first)
+        if ['NNG', 'NNP'].include?(part.feature.split(',').first) && part.surface.length > 1
           tag_array.push(part.surface)    
         end
       end
       # puts tag_array
-      keyword_frequency_desc = tag_array.each_with_object(Hash.new(0)){ |m,h| h[m] += 1 }.sort_by{ |k,v| v }.reverse
-      tags = []
-      # puts keyword_frequency_desc.first(5).map{|w| w[0] }
-      hash["tags"] = keyword_frequency_desc.first(5).map{|w| w[0] }
-      puts "done and cleaning up"
+      if tag_array.empty?
+        hash["tags"] = [nil]
+      else
+        keyword_frequency_desc = tag_array.each_with_object(Hash.new(0)){ |m,h| h[m] += 1 }.sort_by{ |k,v| v }.reverse
+        tags = []
+        # puts keyword_frequency_desc.first(5).map{|w| w[0] }
+        hash["tags"] = keyword_frequency_desc.first(5).map{|w| w[0] }
+      end
+      puts "[Hugo Contents Directories 2 MARKDOWN] natto done and cleaning up"
       File.delete(path + ".txt") if File.exists? (path + ".txt")
-      puts "tag process done"
-      puts
+      puts "[Hugo Contents Directories 2 MARKDOWN] tag process done"
+      puts "[Hugo Contents Directories 2 MARKDOWN]"
+    else
+      hash["tags"] = [nil]
     end
       
     extension = File.extname(path)
@@ -122,8 +140,12 @@ all_children.each_with_index do |path, i|
       f.write(YAML.dump(JSON.parse(hash.to_json)))
       f << "---\n"
     end
+    puts "[Hugo Contents Directories 2 MARKDOWN] Created \"#{md_file_path}\""
+    puts "[Hugo Contents Directories 2 MARKDOWN]"
+
   else
-    puts "[Hugo Contents Directories 2 JSON] :::WARNING::: Check if #{path} is a valid path."
+    puts "[Hugo Contents Directories 2 MARKDOWN] :::WARNING::: Check if #{path} is a valid path."
     next
   end
 end
+puts "[Hugo Contents Directories 2 MARKDOWN] Script End."
